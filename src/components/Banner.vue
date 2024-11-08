2f0f344d67d988999d3909c644815f221da614e9
B2005848 <namltgt2002@gmail.com>
2024-11-08 23:15:28 +0700

    update

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,76 +1,78 @@
 <template>
-  <div
-    class="d-flex align-items-center"
-    style="height: 25px; justify-content: space-evenly"
-  >
-    <div>
-      <span>Hotline:</span>
-      <span style="color: #1b51a3; font-weight: bold; margin-left: 10px"
-        >1800 xxxx</span
-      >
-    </div>
+  <div>
+    <div
+      class="d-flex align-items-center"
+      style="height: 25px; justify-content: space-evenly"
+    >
+      <div>
+        <span>Hotline:</span>
+        <span style="color: #1b51a3; font-weight: bold; margin-left: 10px"
+          >1800 xxxx</span
+        >
+      </div>
 
-    <div class="blink">
-      Deal hot trong tháng
-      <span
-        style="
-          background-color: yellowgreen;
-          color: #ffffff;
-          font-size: 10px;
-          padding: 0px 5px 0px 5px;
-        "
-        >New</span
-      >
-    </div>
-    <div>
-      Góc sức khỏe
-      <span
-        style="
-          background-color: yellowgreen;
-          color: #ffffff;
-          font-size: 10px;
-          padding: 0px 5px 0px 5px;
-        "
-        >New</span
-      >
-    </div>
+      <div class="blink">
+        Deal hot trong tháng
+        <span
+          style="
+            background-color: yellowgreen;
+            color: #ffffff;
+            font-size: 10px;
+            padding: 0px 5px 0px 5px;
+          "
+          >New</span
+        >
+      </div>
+      <div>
+        Góc sức khỏe
+        <span
+          style="
+            background-color: yellowgreen;
+            color: #ffffff;
+            font-size: 10px;
+            padding: 0px 5px 0px 5px;
+          "
+          >New</span
+        >
+      </div>
 
-    <div>Chăm sóc khách hàng</div>
+      <div>Chăm sóc khách hàng</div>
 
-    <div>Giao nhanh 2h</div>
-    <div>Hệ thống phòng khám</div>
-  </div>
-  <div class="category d-flex">
-    <div class="flex-1">
-      <select name="category" id="category">
-        <option value="#" selected>DANH MỤC</option>
-        <option v-for="cat in category" :key="cat.id" :value="cat.id">
+      <div>Giao nhanh 2h</div>
+      <div>Hệ thống phòng khám</div>
+    </div>
+    <div class="category d-flex">
+      <div class="flex-1">
+        <select name="category" id="category">
+          <option value="#" selected>DANH MỤC</option>
+          <option v-for="cat in category" :key="cat.id" :value="cat.id">
+            {{ cat.name }}
+          </option>
+        </select>
+      </div>
+
+      <div class="flex-1">
+        <Input_search></Input_search>
+        <a href="" v-for="cat in category" :key="cat.id" :value="cat.id">
           {{ cat.name }}
-        </option>
-      </select>
-    </div>
+        </a>
+      </div>
 
-    <div class="flex-1">
-      <Input_search></Input_search>
-      <a href="" v-for="cat in category" :key="cat.id" :value="cat.id">
-        {{ cat.name }}
-      </a>
-    </div>
-
-    <div style="border-left: #ffffff 2px solid; height: 50px"></div>
-    <div class="flex-1">
-      <span style="color: #ffffff; font-size: 25px; margin-right: 20px">
-        <font-awesome-icon
-          icon=" fa-cart-arrow-down"
-          style="color: #ffffff; font-size: 25px"
-        />(.)
-      </span>
-      <span>
-        <font-awesome-icon
-          icon=" fa-bell"
-          shake
-          style="color: #fbfe23; font-size: 25px"
-      /></span>
+      <div style="border-left: #ffffff 2px solid; height: 50px"></div>
+      <div class="flex-1">
+        <span style="color: #ffffff; font-size: 25px; margin-right: 20px">
+          <font-awesome-icon
+            icon=" fa-cart-arrow-down"
+            style="color: #ffffff; font-size: 25px"
+          />(.)
+        </span>
+        <span>
+          <font-awesome-icon
+            icon=" fa-bell"
+            shake
+            style="color: #fbfe23; font-size: 25px"
+        /></span>
+      </div>
     </div>
   </div>
 </template>
